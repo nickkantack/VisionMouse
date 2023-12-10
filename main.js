a26f9177d9b1712b245678672407d41bcfba04ae
Nick Kantack <kantackn@amazon.com>
2023-12-10 12:36:39 -0500

    Now can move a primitive mouse around the screen using my thumb tip

--- main.js
+++ main.js
@@ -11,11 +11,16 @@ const detectorConfig = {
 }
 const detector = await handPoseDetection.createDetector(model, detectorConfig);
 
+// Create DOM elements
 const player = document.createElement("video");
 player.autoplay = true;
 const debug = document.createElement("div");
 document.body.appendChild(debug);
 document.body.appendChild(player);
+
+const mouse = document.createElement("div");
+mouse.style = `position: absolute; left: 0; top: 0; width: 10px; height: 10px; z-index: 100; background: #000;`;
+document.body.appendChild(mouse);
 
 // Attach the video stream to the video element and autoplay.
 let awaitingPreviousHandEstimation = false;
@@ -27,6 +32,17 @@ navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
         const hands = await detector.estimateHands(player);
         awaitingPreviousHandEstimation = false;
         debug.innerHTML = hands && hands.length > 0 ? hands[0].score : 0;
+        
+        // Move the mouse to the correct location
+        if (hands && hands.length > 0) {
+            const thumbTipKeypoint = [...hands[0].keypoints].filter(x => x.name === "thumb_tip")[0];
+            const cursorLeftPixels = thumbTipKeypoint.x / player.getBoundingClientRect().width * screen.width;
+            const cursorTopPixels = thumbTipKeypoint.y / player.getBoundingClientRect().height * screen.height;
+            mouse.style.transform = `translate(${screen.width - cursorLeftPixels}px, ${cursorTopPixels}px)`;
+        }
+
+        // Use document.elementFromPoint(x, y).click(); to click things
+
         requestAnimationFrame(callback);
     };
     setTimeout(() => {
